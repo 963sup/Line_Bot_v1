a5f61523e81a4eb204b7c2c4cc18b7a99711e793
963sup <963sup@gmail.com>
2026-09-27 03:12:29 +0800

    feat(web): compose public repository projection

---
+++ apps/web/src/app/api/_composition/repository-public.server.ts
@@ -0,0 +1,12 @@
+import { PostgresPublicRepositoryStore } from "@line-work/repository/adapters/postgres/public";
+import { createPublicRepositories } from "@line-work/repository/application/public";
+
+const state = globalThis as typeof globalThis & {
+  publicRepositoryStore?: PostgresPublicRepositoryStore;
+};
+
+function publicRepositoryStore() {
+  return (state.publicRepositoryStore ??= new PostgresPublicRepositoryStore());
+}
+
+export const publicRepositories = createPublicRepositories(publicRepositoryStore());
