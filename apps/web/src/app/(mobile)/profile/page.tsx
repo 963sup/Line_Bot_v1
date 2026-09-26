55297e32c3fe03882e452797008c123a09aa2080
963sup <963sup@gmail.com>
2026-09-27 03:53:32 +0800

    style(web): organize profile page imports

--- apps/web/src/app/(mobile)/profile/page.tsx
+++ apps/web/src/app/(mobile)/profile/page.tsx
@@ -1,6 +1,6 @@
-import ProfileHub from "./profile-hub";
 import { lineMiniApp } from "../../../shared/server/line-mini-app";
 import AppShell from "../_shell/app-shell";
+import ProfileHub from "./profile-hub";
 
 export const dynamic = "force-dynamic";
 
