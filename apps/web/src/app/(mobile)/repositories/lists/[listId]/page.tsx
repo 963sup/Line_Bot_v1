8192bc723c276c783469f9801962fe08af4d7c5b
963sup <963sup@gmail.com>
2026-09-27 01:58:05 +0800

    style(web): normalize Star List detail page

--- apps/web/src/app/(mobile)/repositories/lists/[listId]/page.tsx
+++ apps/web/src/app/(mobile)/repositories/lists/[listId]/page.tsx
@@ -1,16 +1,12 @@
-import RepositoryStarListDetail from "../../../../../modules/repository/star-list-detail";
 import { repositoryStarListsPath } from "../../../../../modules/repository/resource-navigation";
+import RepositoryStarListDetail from "../../../../../modules/repository/star-list-detail";
 import { lineMiniApp } from "../../../../../shared/server/line-mini-app";
 import { PageHeading } from "../../../../../shared/ui/page-layout";
 import AppShell from "../../../_shell/app-shell";
 
 export const dynamic = "force-dynamic";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ listId: string }>;
-}) {
+export default async function Page({ params }: { params: Promise<{ listId: string }> }) {
   const { listId } = await params;
   return (
     <AppShell>
